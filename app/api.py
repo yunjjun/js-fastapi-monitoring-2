7e15710f4ebf6b721e3a7cdf9fec9b220e7aa834
yunjjun <yunjjun0504@gmail.com>
2021-12-04 21:03:10 +0900

    first commit

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import numpy as np
 from fastapi import FastAPI, Response
 from joblib import load
-from schemas import Wine, Rating
+from app.schemas import Wine, Rating
 
 ROOT_DIR = Path(__file__).parent.parent
 
